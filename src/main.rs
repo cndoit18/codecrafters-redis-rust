f24487fba188ff337b21741daffd6c19b2fd6558
cndoit18 <cndoit18@outlook.com>
2024-04-16 13:27:45 +0800

    finish stage 3

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::{
     io::{Read, Write},
-    net::TcpListener,
+    net::{TcpListener, TcpStream},
 };
 
 fn main() {
@@ -10,13 +10,23 @@ fn main() {
         match stream {
             Ok(mut stream) => {
                 println!("accepted new connection");
-                let mut buf = [0; 1024];
-                stream.read(&mut buf).unwrap();
-                stream.write(b"+PONG\r\n").unwrap();
+                handle_connection(&mut stream);
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
+    }
+}
+
+fn handle_connection(stream: &mut TcpStream) {
+    let mut buf = [0_u8; 1024];
+    while let Ok(n) = stream.read(&mut buf) {
+        if n == 0 {
+            break
+        }
+        stream
+                .write_all(b"+PONG\r\n")
+                .expect("writing PONG response");
     }
 }
