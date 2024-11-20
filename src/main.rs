111f352a225c8d5cf19a07bebef69f2eaa6fcb89
cndoit18 <cndoit18@outlook.com>
2024-11-20 14:03:07 +0800

    respond to multiple PINGs
    Change-Id: I2784d952240186c5f972eafc82595c9c1459c6cc

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
+use std::io::Read;
 use std::io::Write;
-use std::net::TcpListener;
+use std::net::{TcpListener, TcpStream};
 
 fn main() {
     println!("starting server...");
@@ -7,13 +8,20 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(mut stream) => {
+            Ok(stream) => {
                 println!("accepted new connection");
-                stream.write_all(b"+PONG\r\n").unwrap();
+                handle_connection(stream);
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
+    }
+}
+fn handle_connection(mut stream: TcpStream) {
+    let mut buf = [0_u8; 1024];
+    loop {
+        let _ = stream.read(&mut buf).unwrap();
+        stream.write_all(b"+PONG\r\n").unwrap();
     }
 }
