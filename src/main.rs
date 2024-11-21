e7185391d034bd26bc9dce17c7aa927d71666c97
cndoit18 <cndoit18@outlook.com>
2024-11-21 20:23:34 +0800

    expiry
    Change-Id: I2691373a34370493b76f92b2cbb0412927a2ac53

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,15 @@ use std::net::{TcpListener, TcpStream};
 use std::sync::Arc;
 use std::sync::Mutex;
 use std::thread;
+use std::time::Duration;
+use std::time::SystemTime;
+use std::time::UNIX_EPOCH;
 
 fn main() {
     println!("starting server...");
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
-    let data = Arc::new(Mutex::new(HashMap::<String, String>::new()));
+    let data = Arc::new(Mutex::new(HashMap::<String, Val>::new()));
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
@@ -30,7 +33,7 @@ fn main() {
         }
     }
 }
-fn handle_connection(mut stream: TcpStream, mut data: Arc<Mutex<HashMap<String, String>>>) {
+fn handle_connection(mut stream: TcpStream, mut data: Arc<Mutex<HashMap<String, Val>>>) {
     let mut buf = [0_u8; 1024];
     while let Ok(n) = stream.read(&mut buf) {
         if n == 0 {
@@ -56,7 +59,7 @@ fn handle_connection(mut stream: TcpStream, mut data: Arc<Mutex<HashMap<String, 
 
 fn command_handle(
     v: &[Value],
-    data: &mut Arc<Mutex<HashMap<String, String>>>,
+    data: &mut Arc<Mutex<HashMap<String, Val>>>,
     stream: &mut TcpStream,
 ) {
     if v.is_empty() {
@@ -66,29 +69,67 @@ fn command_handle(
     if let Value::String(s) = v.first().unwrap() {
         match s.to_lowercase().as_str() {
             "echo" => {
-                if let Value::String(v) = v.get(1).unwrap() {
-                    stream.write_all(format! {"+{}\r\n", v}.as_bytes()).unwrap();
+                if let [_, Value::String(value)] = v {
+                    stream
+                        .write_all(format! {"+{}\r\n", value}.as_bytes())
+                        .unwrap();
                 }
             }
             "set" => {
-                if let Value::String(key) = v.get(1).unwrap() {
-                    if let Value::String(value) = v.get(2).unwrap() {
-                        *data
-                            .lock()
-                            .unwrap()
-                            .entry(key.to_string())
-                            .or_insert(value.to_string()) = value.to_string();
-                        stream.write_all(b"+OK\r\n").unwrap();
+                if let [_, Value::String(key), Value::String(value)] = v {
+                    *data
+                        .lock()
+                        .unwrap()
+                        .entry(key.to_string())
+                        .or_insert(Val::String(value.to_string())) = Val::String(value.to_string());
+                    stream.write_all(b"+OK\r\n").unwrap();
+                }
+                if let [_, Value::String(key), Value::String(value), Value::String(pt), Value::String(duration)] =
+                    v
+                {
+                    if pt.to_ascii_lowercase() != "px" {
+                        stream.write_all(b"$-1\r\n").unwrap();
+                        return;
                     }
+                    let now = SystemTime::now()
+                        .checked_add(Duration::from_millis(duration.parse::<u64>().unwrap()))
+                        .unwrap()
+                        .duration_since(UNIX_EPOCH)
+                        .unwrap()
+                        .as_millis();
+                    println!("expire: {:?}", now);
+                    *data
+                        .lock()
+                        .unwrap()
+                        .entry(key.to_string())
+                        .or_insert(Val::Expiry(value.to_string(), now)) =
+                        Val::Expiry(value.to_string(), now);
+                    stream.write_all(b"+OK\r\n").unwrap();
                 }
             }
             "get" => {
-                if let Value::String(key) = v.get(1).unwrap() {
+                if let [_, Value::String(key)] = v {
                     if data
                         .lock()
                         .unwrap()
                         .get(key)
-                        .map(|v| stream.write_all(format! {"+{}\r\n",v}.as_bytes()).unwrap())
+                        .map(|v| match v {
+                            Val::String(s) => {
+                                stream.write_all(format! {"+{}\r\n",s}.as_bytes()).unwrap();
+                            }
+                            Val::Expiry(s, duration)
+                                if &SystemTime::now()
+                                    .duration_since(UNIX_EPOCH)
+                                    .unwrap()
+                                    .as_millis()
+                                    < duration =>
+                            {
+                                stream.write_all(format! {"+{}\r\n",s}.as_bytes()).unwrap();
+                            }
+                            _ => {
+                                stream.write_all(b"$-1\r\n").unwrap();
+                            }
+                        })
                         .is_none()
                     {
                         stream.write_all(b"$-1\r\n").unwrap();
@@ -100,4 +141,9 @@ fn command_handle(
             }
         }
     }
+}
+
+enum Val {
+    String(String),
+    Expiry(String, u128),
 }
